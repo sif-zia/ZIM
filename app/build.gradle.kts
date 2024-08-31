ec6816c4ce421a43858fce80f32c1ee560d0eb1f
Zainab Bilal <zainabilalmunawar@gmail.com>
2024-08-31 21:14:06 +0500

    Added Room dependencies in Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    kotlin("kapt")
 }
 
 android {
@@ -35,6 +36,7 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+        freeCompilerArgs = listOf("-Xjvm-default=all-compatibility")
     }
     buildFeatures {
         compose = true
@@ -66,4 +68,19 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Navigation Compose
+    implementation("androidx.navigation:navigation-compose:2.5.0")
+
+    // compose viewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+
+    //room
+    val room_version = "2.4.2"
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+    testImplementation("androidx.room:room-testing:$room_version")
+    androidTestImplementation("androidx.room:room-testing:$room_version")
+
 }
---
+++ app/src/main/java/com/example/zim/data/room/models/Models.kt
@@ -0,0 +1,2 @@
+package com.example.zim.data.room.models
+
