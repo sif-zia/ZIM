efc7b86f259730ecf9cb12ddfcf54ff0213d74a1
Sif Zia <sifzia@yahoo.com>
2024-08-31 16:42:35 +0500

    Removed Warnings

--- app/src/main/java/com/example/zim/ui/theme/Color.kt
+++ app/src/main/java/com/example/zim/ui/theme/Color.kt
@@ -2,19 +2,19 @@ package com.example.zim.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+//val Purple80 = Color(0xFFD0BCFF)
+//val PurpleGrey80 = Color(0xFFCCC2DC)
+//val Pink80 = Color(0xFFEFB8C8)
+//
+//val Purple40 = Color(0xFF6650a4)
+//val PurpleGrey40 = Color(0xFF625b71)
+//val Pink40 = Color(0xFF7D5260)
 
 val DarkGreenishGray = Color(0xFF2E3938)
 val LightGray = Color(0xFFCCCECE)
 val Purple = Color(0xFF7154BD)
-val Green = Color(0xFF34DB25)
-
-val FadedDarkGreenishGray = Color(0xBF2E3938)
-val FadedLightGray = Color(0x7FCCCECE)
-val FadedPurple = Color(0x3F7154BD)
+//val Green = Color(0xFF34DB25)
+//
+//val FadedDarkGreenishGray = Color(0xBF2E3938)
+//val FadedLightGray = Color(0x7FCCCECE)
+//val FadedPurple = Color(0x3F7154BD)
