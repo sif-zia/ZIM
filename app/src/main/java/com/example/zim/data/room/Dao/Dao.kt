820790bb6e9de9c732085ccc07b9d39bf9b3287a
Zainab Bilal <zainabilalmunawar@gmail.com>
2024-09-06 23:20:19 +0500

    Created DAOs (database)

--- app/src/main/java/com/example/zim/data/room/Dao/Dao.kt
+++ app/src/main/java/com/example/zim/data/room/Dao/Dao.kt
@@ -1,5 +1,52 @@
 package com.example.zim.data.room.Dao
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.zim.data.room.models.Users
+import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface UserDao{
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    suspend fun insert(users:Users)
 
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun update(user:Users)
+
+    @Delete
+    suspend fun delete(users:Users)
+
+    @Query("""
+        SELECT * 
+        FROM Users as U INNER JOIN Current_User as Crr
+        ON Crr.User_ID = U.User_ID
+    """)
+    fun getCrrUser():Flow<Users>
+
+    @Query("""
+        SELECT *
+        FROM Users as U 
+        INNER JOIN Connections as Con 
+        INNER JOIN Current_User as Crr
+        WHERE (Con.UserA_ID = Crr.User_ID AND Con.UserB_ID = U.User_ID) 
+        OR (Con.UserB_ID = Crr.User_ID AND Con.UserA_ID = U.User_ID)
+    """)
+    fun getConUsers():Flow<List<Users>>
 }
+
+@Dao
+interface Messages{
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    suspend fun insert(messages: Messages)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun update(messages: Messages)
+
+    @Delete
+    suspend fun delete(messages: Messages)
+}
+
+
--- app/src/main/java/com/example/zim/data/room/models/Models.kt
+++ app/src/main/java/com/example/zim/data/room/models/Models.kt
@@ -2,6 +2,7 @@ package com.example.zim.data.room.models
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -18,35 +19,87 @@ data class Users(
     val cover: String? = null // URI
 )
 
-@Entity(tableName = "Current_User")
+@Entity(
+    tableName = "Current_User",
+    foreignKeys = [
+        ForeignKey(
+            entity = Users::class,
+            parentColumns = arrayOf("User_ID"),
+            childColumns = arrayOf("User_ID"),
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        )]
+)
 data class CrrUser(
     @ColumnInfo(name = "CrrUser_ID")
     @PrimaryKey(autoGenerate = true)
     val id: Int = 1,
     val pKey: String, // Any
-    val userIdFk:Int,
+    @ColumnInfo(name = "User_ID")
+    val userIdFk: Int,
 )
 
-@Entity(tableName = "Connections")
+@Entity(
+    tableName = "Connections",
+    foreignKeys = [
+        ForeignKey(
+            entity = Users::class,
+            parentColumns = arrayOf("User_ID"),
+            childColumns = arrayOf("UserA_ID"),
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Users::class,
+            parentColumns = arrayOf("User_ID"),
+            childColumns = arrayOf("UserB_ID"),
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        )],
+
+    )
 data class Connections(
     @ColumnInfo(name = "Con_ID")
     @PrimaryKey(autoGenerate = true)
     val id: Int = 1,
-    val status:String, // Connected, Not Connected
-    val userAIdFk:Int,
-    val userBIdFk:Int,
+    val status: String, // Connected, Not Connected
+    @ColumnInfo(name = "UserA_ID")
+    val userAIdFk: Int,
+    @ColumnInfo(name = "UserB_ID")
+    val userBIdFk: Int,
 )
 
-@Entity(tableName = "Keys")
+@Entity(
+    tableName = "Keys",
+    foreignKeys = [
+        ForeignKey(
+            entity = Users::class,
+            parentColumns = arrayOf("User_ID"),
+            childColumns = arrayOf("User_ID"),
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        )]
+)
 data class Keys(
     @ColumnInfo(name = "Key_ID")
     @PrimaryKey(autoGenerate = true)
     val id: Int = 1,
     val key: String, // Any
+    @ColumnInfo(name = "User_ID")
     val ownerIdFk: Int,
 )
 
-@Entity(tableName = "Messages")
+@Entity(
+    tableName = "Messages",
+    foreignKeys = [
+        ForeignKey(
+            entity = Messages::class,
+            parentColumns = arrayOf("Msg_ID"),
+            childColumns = arrayOf("ReplyMsg_ID"),
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.SET_NULL
+        )]
+)
 data class Messages(
     @ColumnInfo(name = "Msg_ID")
     @PrimaryKey(autoGenerate = true)
@@ -56,27 +109,63 @@ data class Messages(
     val sentTime: LocalDateTime,
     val isSent: Boolean,
     val isForwarded: Boolean,
-    val groupIdFk: Int,
-    val replyOfFk: Int?=null,
+    @ColumnInfo(name = "ReplyMsg_ID")
+    val replyOfFk: Int? = null,
 )
 
-@Entity(tableName = "Sent_Messages")
+@Entity(tableName = "Sent_Messages",
+    foreignKeys = [
+        ForeignKey(
+            entity = Messages::class,
+            parentColumns = arrayOf("Msg_ID"),
+            childColumns = arrayOf("SMsg_ID"),
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Users::class,
+            parentColumns = arrayOf("User_ID"),
+            childColumns = arrayOf("User_ID"),
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        )
+    ])
 data class SentMessages(
     @ColumnInfo(name = "SMsg_ID")
     @PrimaryKey(autoGenerate = true)
     val id: Int = 1,
     val status: String, // Failed, Sent, Sending
+    @ColumnInfo(name = "Msg_ID")
     val msgIdFk: Int,
+    @ColumnInfo(name = "User_ID")
     val receiverIdFk: Int,
 )
 
-@Entity(tableName = "Received_Messages")
+@Entity(tableName = "Received_Messages",
+    foreignKeys = [
+        ForeignKey(
+            entity = Messages::class,
+            parentColumns = arrayOf("Msg_ID"),
+            childColumns = arrayOf("RMsg_ID"),
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Users::class,
+            parentColumns = arrayOf("User_ID"),
+            childColumns = arrayOf("User_ID"),
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        )
+    ])
 data class ReceivedMessages(
     @ColumnInfo(name = "RMsg_ID")
     @PrimaryKey(autoGenerate = true)
     val id: Int = 1,
     val receivedTime: LocalDateTime,
+    @ColumnInfo(name = "Msg_ID")
     val msgIdFk: Int,
+    @ColumnInfo(name = "User_ID")
     val senderIdFk: Int,
 )
 
@@ -91,14 +180,32 @@ data class Alerts(
     val sentTime: LocalDateTime,
 )
 
-@Entity(tableName = "Received_Alerts")
+@Entity(tableName = "Received_Alerts",
+    foreignKeys = [
+        ForeignKey(
+            entity = Alerts::class,
+            parentColumns = arrayOf("Alert_ID"),
+            childColumns = arrayOf("RAlert_ID"),
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Users::class,
+            parentColumns = arrayOf("User_ID"),
+            childColumns = arrayOf("User_ID"),
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        )
+    ])
 data class ReceivedAlerts(
     @ColumnInfo(name = "RAlert_ID")
     @PrimaryKey(autoGenerate = true)
     val id: Int = 1,
     val hops: Int,
     val receivedTime: LocalDateTime,
+    @ColumnInfo(name = "Alert_ID")
     val alertIdFk: Int,
+    @ColumnInfo(name = "User_ID")
     val initiatorIdFk: Int,
 )
 
